971e9fbc97c1e669c805042026400fc5cf4212d8
siddharth-1407 <sanksiddharth@gmail.com>
2023-06-05 22:06:39 +0530

    Add dist directory

---
+++ dist/assets/ChannelCommunity-3db75b03.js
@@ -0,0 +1,1 @@
+import{a as n}from"./index-97ce0fac.js";const a=()=>n("div",{className:"2xl:min-w-[80.25rem]",children:"ChannelCommunity"});export{a as default};
--- dist/assets/ChannelCommunity-c5c2601e.js
+++
@@ -1,1 +0,0 @@
-import{a as n}from"./index-4483838d.js";const a=()=>n("div",{className:"2xl:min-w-[80.25rem]",children:"ChannelCommunity"});export{a as default};
