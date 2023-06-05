375597a5bf24f10653cb27b86ae1bc61b9f7f8ac
siddharth-1407 <113137057+siddharth-1407@users.noreply.github.com>
2023-06-05 22:11:33 +0530

    Delete dist directory

--- dist/assets/ChannelCommunity-3db75b03.js
+++
@@ -1,1 +0,0 @@
-import{a as n}from"./index-97ce0fac.js";const a=()=>n("div",{className:"2xl:min-w-[80.25rem]",children:"ChannelCommunity"});export{a as default};
--- dist/assets/IsLive-22a45e03.js
+++
@@ -1,1 +0,0 @@
-import{j as s,a as e}from"./index-97ce0fac.js";import{S as t}from"./Live-5fe53068.js";const i=()=>s("div",{className:"absolute bottom-1 right-1 bg-[#cc0000e6] text-white text-xs font-bold rounded-[0.3rem] flex gap-1 items-center px-1 py-[0.1rem]",children:[e(t,{className:"fill-white"}),e("span",{className:"uppercase",children:"Live"})]});export{i as default};
