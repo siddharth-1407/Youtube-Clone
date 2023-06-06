ff3b2cbfb5fa392c47e2a00dbef7b7a53504b1b7
siddharth-1407 <113137057+siddharth-1407@users.noreply.github.com>
2023-06-06 17:09:08 +0530

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -115,7 +115,7 @@ function App() {
 							<Routes>
 								<Route
 									exact
-									path='/Youtube-Clone/
+									path='/Youtube-Clone/'
 									element={
 										<Suspense fallback={<VideoCardSkeletonFallback />}>
 											<Feed />
