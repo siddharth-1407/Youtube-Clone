9b0d7f934ab063bec4e492cf255b580dd61eef50
siddharth-1407 <113137057+siddharth-1407@users.noreply.github.com>
2023-06-07 19:25:36 +0530

    Update ChannelDetails.jsx

--- src/Components/ChannelsPage/ChannelDetails.jsx
+++ src/Components/ChannelsPage/ChannelDetails.jsx
@@ -1,9 +1,12 @@
-import React from 'react';
+import React, { useContext, useEffect } from 'react';
+import { Context } from '../../context/ContextApi';
 import ChannelProfile from './ChannelProfile';
 import ChannelNavbar from './ChannelNavbar';
 import ChannelContent from './ChannelContent/ChannelContent';
 
 const ChannelDetails = () => {
+	const { channelData } = useContext(Context);
+	useEffect(() => {}, [channlData]);
 	return (
 		<div className='flex  flex-col w-full'>
 			<ChannelProfile />
